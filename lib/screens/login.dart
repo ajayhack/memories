0aee3dff36ee65a6cf33bb2f1b006b79c0879b4d
ajay.thakur <ajay49811@gmail.com>
2020-10-28 18:03:30 +0530

    1.Facebook Social Login and Gmail Social Login Integrated.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget{
   @override
@@ -9,7 +9,15 @@ class Login extends StatefulWidget{
   }
 }
 
-class UserLoginScreen extends State<Login>{
+class UserLoginScreen extends State<Login> {
+  static final FacebookLogin facebookSignIn = new FacebookLogin();
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: <String>[
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,40 +32,78 @@ class UserLoginScreen extends State<Login>{
   //Below method is used to show Memories App login Screen:-
   getDisplayLoginView(){
     return Center(
-      child: RaisedButton(
-        textColor: Colors.white,
-        color: Colors.blue,
-        onPressed: () {
-          doLogin();
-        },
-        child: Text(
-          'Login via Facebook',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              fontSize: 16.0, fontStyle: FontStyle.normal),
-        ),
-      )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              onPressed: () {
+                _facebookLogin();
+              },
+              child: Text(
+                'Login via Facebook',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    fontSize: 16.0, fontStyle: FontStyle.normal),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 0, top: 16, right: 0, bottom: 0),
+                child: RaisedButton
+                  (
+                  textColor: Colors.white,
+                  color: Colors.green,
+                  onPressed: () {
+                    _gmailLogin();
+                  },
+                  child: Text(
+                    'Login via Gmail',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        fontSize: 16.0, fontStyle: FontStyle.normal),
+                  ),
+                )
+            ),
+          ],
+        )
     );
   }
 
   //Below method is used to start login via Facebook:-
-  void doLogin() async{
-    var facebookLogin = FacebookLogin();
-    var facebookLoginResult =
-        await facebookLogin.logInWithReadPermissions(['email']);
-    switch (facebookLoginResult.status) {
-      case FacebookLoginStatus.error:
-        print("Error");
-        //onLoginStatusChanged(false);
+  Future<Null> _facebookLogin() async {
+    final FacebookLoginResult result =
+    await facebookSignIn.logIn(['email']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final FacebookAccessToken accessToken = result.accessToken;
+        /*_showMessage('''
+         Logged in!
+         Token: ${accessToken.token}
+         User id: ${accessToken.userId}
+         Expires: ${accessToken.expires}
+         Permissions: ${accessToken.permissions}
+         Declined permissions: ${accessToken.declinedPermissions}
+         ''');*/
         break;
       case FacebookLoginStatus.cancelledByUser:
-        print("CancelledByUser");
-        //onLoginStatusChanged(false);
+      //_showMessage('Login cancelled by the user.');
         break;
-      case FacebookLoginStatus.loggedIn:
-        print("LoggedIn");
-        //onLoginStatusChanged(true);
+      case FacebookLoginStatus.error:
+      /*_showMessage('Something went wrong with the login process.\n'
+            'Here\'s the error Facebook gave us: ${result.errorMessage}');*/
         break;
     }
   }
+
+  //Below method is used to make social login via GMAIL:-
+  Future<void> _gmailLogin() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
 }
