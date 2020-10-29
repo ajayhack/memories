302b861180485714703e60249cde3a3bc08762c1
ajay.thakur <ajay49811@gmail.com>
2020-10-29 19:09:55 +0530

    1.Facebook Social Login and Gmail Social Login Integrated.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,6 +11,9 @@ class Login extends StatefulWidget{
 
 class UserLoginScreen extends State<Login> {
   static final FacebookLogin facebookSignIn = new FacebookLogin();
+  final mobileNumberController = TextEditingController();
+  final otpController = TextEditingController();
+
   GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: <String>[
       'email',
@@ -35,37 +38,139 @@ class UserLoginScreen extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.blue,
-              onPressed: () {
-                _facebookLogin();
-              },
-              child: Text(
-                'Login via Facebook',
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    fontSize: 16.0, fontStyle: FontStyle.normal),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(left: 0, top: 16, right: 0, bottom: 0),
-                child: RaisedButton
-                  (
-                  textColor: Colors.white,
-                  color: Colors.green,
-                  onPressed: () {
-                    _gmailLogin();
-                  },
+        Padding(
+          padding: EdgeInsets.only(left: 16, top: 0, right: 16, bottom: 0),
+          child: Card(
+            color: Colors.white,
+            elevation: 10.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
                   child: Text(
-                    'Login via Gmail',
+                    'Memories',
                     textDirection: TextDirection.ltr,
                     style: TextStyle(
-                        fontSize: 16.0, fontStyle: FontStyle.normal),
+                      fontSize: 24.0,
+                      fontStyle: FontStyle.normal,
+                      color: Colors.red,
+                    ),
                   ),
-                )
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 0),
+                  child: TextField(
+                    maxLength: 50,
+                    controller: mobileNumberController,
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelText: 'Mobile Number',
+                        labelStyle: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 0),
+                  child: TextField(
+                    obscureText: true,
+                    maxLength: 20,
+                    controller: otpController,
+                    style: TextStyle(color: Colors.black, fontSize: 16.0),
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        fillColor: Colors.white,
+                        filled: true,
+                        labelText: 'OTP',
+                        labelStyle: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 16),
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      onPressed: () {
+                        //doLogin();
+                      },
+                      child: Text(
+                        'Login',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 16.0, fontStyle: FontStyle.normal),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 16),
+          child: Text(
+            'or',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                fontSize: 16.0,
+                fontStyle: FontStyle.normal,
+                color: Colors.grey),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 0, top: 16, right: 0, bottom: 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton.icon(
+                elevation: 10,
+                onPressed: () => _facebookLogin(),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                icon: Image.asset('assets/images/facebook.png'),
+                label: Text(
+                  'Login via Facebook',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(fontSize: 16.0, fontStyle: FontStyle.normal),
+                ),
+                textColor: Colors.blue,
+                splashColor: Colors.red,
+                color: Colors.white,
+              ),
+              Padding(
+                  padding:
+                      EdgeInsets.only(left: 16, top: 0, right: 0, bottom: 0),
+                  child: RaisedButton.icon(
+                    elevation: 10,
+                    onPressed: () => _gmailLogin(),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                    icon: Image.asset('assets/images/gmail.png'),
+                    label: Text(
+                      'Login via Gmail',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 16.0, fontStyle: FontStyle.normal),
+                    ),
+                    textColor: Colors.red,
+                    splashColor: Colors.red,
+                    color: Colors.white,
+                  )),
+            ],
+          ),
+        ),
+      ],
         )
     );
   }
